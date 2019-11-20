d11dd9740f295e888a9d82c65908db80234b7368
kaiquizando <kai@untangledmedia.com>
2019-11-20 12:01:26 +0100

    up to lesson 64, to continue with lesson 65

--- app.js
+++ app.js
@@ -30,8 +30,4 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-// 4.) start server
-const port = 3000;
-app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
-});
+module.exports = app;
